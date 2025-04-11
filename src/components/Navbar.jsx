071a40a97053a044a5f7c2ae272c9a18205e1a49
Jack Folsom <jfol0409@outlook.com>
2025-04-11 15:32:18 -0500

    deployment finally working & fixed search button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
-import Login from '../pages/Login';
 
 const Navbar = () => {
 
