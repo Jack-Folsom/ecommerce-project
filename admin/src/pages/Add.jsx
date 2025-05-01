d8e53f2db5c7fa01e61382af2d38f28791de4b54
Jack Folsom <jfol0409@outlook.com>
2025-05-01 18:02:40 -0500

    completed the backend code and started the admin panel

---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Add = () => {
+  return (
+    <form className='flex flex-col w-full items-start gap-3'>
+      <div>
+        <p className='mb-2'>Upload Image</p>
+        
+        <div className='flex gap-2'>
+            <label htmlFor="image1">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image1" hidden/>
+            </label>
+            <label htmlFor="image2">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image2" hidden/>
+            </label>
+            <label htmlFor="image3">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image3" hidden/>
+            </label>
+            <label htmlFor="image4">
+                <img className='w-20' src={assets.upload_area} alt="" />
+                <input type="file" id="image4" hidden/>
+            </label>
+        </div>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product Name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type Here' required />
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product Description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write Content Here' required />
+      </div>
+
+      <div className='flex flex-col sm: flex-row gap-2 w-full sm:gap-8'>
+
+        <div>
+            <p className='mb-2'>Design</p>
+            <select className='w-full px-3 py-2'>
+                <option value="Persian">Persian</option>
+                <option value="Geometric">Geometric</option>
+                <option value="Solid">Solid</option>
+            </select>
+        </div>
+
+        <div>
+            <p className='mb-2'>Texture</p>
+            <select className='w-full px-3 py-2'>
+                <option value="Plush">Plush</option>
+                <option value="Velvet">Velvet</option>
+                <option value="Shaggy">Shaggy</option>
+            </select>
+        </div>
+
+        <div>
+            <p className='mb-2'>Product Price</p>
+            <input className='w-full px-3 py-2 sm:w-[120px]' type="number" placeholder='$100.00' />
+        </div>
+
+      </div>
+
+      <div>
+        <p>Product Sizes</p>
+        <div>
+            <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>2x3</p>
+            </div>
+
+            <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>4x6</p>
+            </div>
+
+            <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>8x12</p>
+            </div>
+        </div>
+      </div>
+
+    </form>
+  )
+}
+
+export default Add
